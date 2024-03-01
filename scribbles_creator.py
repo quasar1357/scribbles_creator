8d93c5ac3e4afe94df8ac95db6b381f5d4fe6556
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2024-03-01 11:20:51 +0100

    Working: combination of squared of sk and lines

--- scribbles_creator.py
+++ scribbles_creator.py
@@ -4,7 +4,7 @@ import napari
 from scipy.spatial import distance
 from skimage.draw import line
 
-def create_scribble(ground_truth, num_lines=5, scribble_width=3, min_line_pix=10):
+def create_scribble(ground_truth, scribble_width=1, num_squares=5, sq_size=20, min_sq_pix=10, num_lines=5, min_line_pix=10, mode="all"):
     '''
     Generate the scribble annotation for the ground truth.
     Input:
@@ -20,12 +20,12 @@ def create_scribble(ground_truth, num_lines=5, scribble_width=3, min_line_pix=10
         if class_val == 0:
             continue
         # Generate the scribble annotation for the class
-        class_scribble = scribble_class(ground_truth, class_val, num_lines, scribble_width, min_line_pix)
+        class_scribble = scribble_class(ground_truth, class_val, scribble_width, num_squares, sq_size, min_sq_pix, num_lines, min_line_pix, mode)
         # Add the scribble annotation of this class to the full scribble (which is valid, because there is no overlap between the classes)
         scribble += class_scribble.astype(np.uint8)
     return scribble
 
-def scribble_class(gt, class_val, num_lines=5, scribble_width=3, min_line_pix=10):
+def scribble_class(gt, class_val, scribble_width=1, num_squares=5, sq_size=20, min_sq_pix=10, num_lines=5, min_line_pix=10, mode="all"):
     '''
     Generate the scribble annotation for a specific class in the ground truth.
     Input:
@@ -40,80 +40,140 @@ def scribble_class(gt, class_val, num_lines=5, scribble_width=3, min_line_pix=10
     # Initialize the scribble for the class with zeros
     class_scribble = np.zeros_like(gt, dtype=np.int32)
     # Generate the primary and secondary skeleton for the class in this slice
-    prim_sk, sec_sk = double_sk_class_2d(gt_class_mask)
+    prim_sk, sec_sk = double_sk_class(gt_class_mask)
     # Check if a skeleton was created, raise an error if not
     if np.sum(sec_sk) == 0:
-        raise ValueError(f"No skeleton was created for class {class_val} in slice {i}.")
-    # Create lines leading from the skeleton to the edge of the mask
-    lines_mask = create_lines(prim_sk, gt_class_mask, num_lines, min_line_pix)
-    # lines_mask = create_lines(sec_sk, gt_class_mask, num_lines)
-    # class_slice_scribble_mask = np.logical_or(lines_mask, sec_sk)
-    class_slice_scribble_mask = lines_mask
-    # Dilate the scribble to make them wider
-    class_slice_scribble_mask = dilation(class_slice_scribble_mask, square(scribble_width))
-    # Add the scribble to this slice of the overall scribble
-    # class_scribble[i] = class_slice_scribble_mask * class_val
-    class_scribble = class_slice_scribble_mask * class_val
-    
-    # v = napari.Viewer()
-    # v.add_image(gt_class_mask)
-    # v.add_labels(prim_sk)
-    # v.add_labels(sec_sk)
+        raise ValueError(f"No skeleton was created for class {class_val}.")
+    # Pick random squares from the skeletons
+    prim_sk_squares = pick_sk_squares(prim_sk, num_squares=num_squares, sq_size=sq_size, min_sq_pix=min_sq_pix)
+    sec_sk_squares = pick_sk_squares(sec_sk, num_squares=num_squares, sq_size=sq_size, min_sq_pix=min_sq_pix)
 
-    # class_scribble = dilation(class_scribble, square(scribble_width))
+    # Create lines leading from the skeleton to the edge of the mask
+    lines = create_lines(prim_sk, gt_class_mask, num_lines, min_line_pix)
+    # lines = create_lines(sec_sk, gt_class_mask, num_lines)
+    if mode == "lines": class_scribble_mask = lines
+    elif mode == "prim_sk": class_scribble_mask = prim_sk_squares
+    elif mode == "sec_sk": class_scribble_mask = sec_sk_squares
+    elif mode == "all": class_scribble_mask = np.logical_or(prim_sk_squares, sec_sk_squares, lines)
+
+    # Dilate the scribble to make them wider
+    class_scribble_mask = dilation(class_scribble_mask, square(scribble_width))
+    # Add the scribble as class value to the overall scribble
+    class_scribble = class_scribble_mask * class_val
     return class_scribble
 
-def double_sk_class_2d(gt_mask_2d, closing_first=0):
+def double_sk_class(gt_mask, closing_prim=0, closing_sec=0):
     '''
-    Create a skeleton as well as a secondary skeleton of the ground truth
+    Create a skeleton as well as a secondary skeleton of the ground truth.
     Input:
-        gt_mask_2d (numpy array): the ground truth mask
-        closing_first (int): the size of the structuring element for the closing operation
+        gt_mask (numpy array): the ground truth mask
+        closing_prim (int): the size of the structuring element for the closing operation on the primary skeleton
+        closing_sec (int): the size of the structuring element for the closing operation on the secondary skeleton
     Output:
         prim_sk (numpy array): the primary skeleton
         sec_sk (numpy array): the secondary skeleton
     '''
-    # Create a skeleton of the ground truth
-    gt_mask2d_with_prim_sk = gt_mask_2d.copy()
-    prim_sk = skeletonize(gt_mask_2d, method='lee') != 0
-    if closing_first: prim_sk = binary_closing(prim_sk, square(closing_first))
+    # Prepare a copy of the ground truth mask for the secondary skeleton
+    gt_mask2d_with_prim_sk = gt_mask.copy()
+    # Create a (primary) skeleton of the ground truth
+    prim_sk = skeletonize(gt_mask, method='lee') != 0
+    if closing_prim: prim_sk = binary_closing(prim_sk, square(closing_prim))
     
-    # Dilate the primary skeleton before generating the secondary skeleton (otherwise it interprets it as gaps in the primary skeleton)
+    # Create a dilated version of the primary skeleton for generating the secondary skeleton (otherwise it interprets it as gaps in the primary skeleton)
     prim_sk_dilated = binary_dilation(prim_sk, square(3))
-
     # Add the dilated skeleton to the ground truth mask
     prim_sk_mask = prim_sk_dilated == 1
     gt_mask2d_with_prim_sk[prim_sk_mask] = False
-
     # Create the secondary skeleton
     sec_sk = skeletonize(gt_mask2d_with_prim_sk, method='lee') != 0
-    # sec_sk = binary_closing(sec_sk, square(3))
+    if closing_sec: sec_sk = binary_closing(sec_sk, square(closing_prim))
 
     return prim_sk, sec_sk
 
-def create_lines(sk_2d, gt_mask_2d, num_lines, min_line_pix=10):
-    all_lines = np.zeros_like(gt_mask_2d, dtype=np.bool8)
+def pick_sk_squares(sk, num_squares=5, sq_size=20, min_sq_pix=10):
+    '''
+    Pick random squares from the skeleton.
+    Input:
+        sk (numpy array): the skeleton
+        num_squares (int): the number of squares to be picked
+        min_sq_pix (int): the minimum number of pixels for a square
+    Output:
+        all_squares (numpy array): the mask of all squares
+    '''
+    all_squares = np.zeros_like(sk, dtype=np.bool8)
+    num_created_squares = 0
+    attempts = 0
+    while num_created_squares < num_squares:
+        attempts += 1
+        square = pick_square(sk, sq_size)
+        if np.sum(square) < min_sq_pix:
+            continue
+        else:
+            all_squares = np.logical_or(all_squares, square)
+            num_created_squares += 1
+        if attempts > np.sum(sk) * num_squares:
+            print("Warning: Could not create all squares from the skeleton ({sk}).")
+            break
+    return all_squares
+
+def pick_square(sk, sq_size=20):
+    '''
+    Take a random point on the skeleton (= True in the mask) and return the skeleton iside a square around it.
+    Input:
+        sk (numpy array): the skeleton mask
+        num_squares (int): the number of squares to be drawn
+        sq_size (int): the size of the square
+    Output:
+        square_mask (numpy array): the skeleton mask inside the square
+    '''
+    # Choose a random point from the skeleton
+    sk_coordinates = np.argwhere(sk)
+    total_points = sk_coordinates.shape[0]
+    random_point_index = np.random.choice(total_points)
+    random_point = sk_coordinates[random_point_index]
+    # Create an empty image to draw the square on
+    square_mask = np.zeros_like(sk)
+    # Draw the square on the image
+    square_mask[random_point[0]-sq_size//2:random_point[0]+sq_size//2, random_point[1]-sq_size//2:random_point[1]+sq_size//2] = sk[random_point[0]-sq_size//2:random_point[0]+sq_size//2, random_point[1]-sq_size//2:random_point[1]+sq_size//2]
+    return square_mask
+
+def create_lines(sk, gt_mask, num_lines=5, min_line_pix=10):
+    '''
+    Create lines leading from a skeleton to the edge of the mask.
+    Input:
+        sk (numpy array): the skeleton mask
+        gt_mask (numpy array): the ground truth mask
+        num_lines (int): the number of lines to be drawn
+    Output:
+        all_lines (numpy array): the mask of all lines
+    '''
+    # Initialize the mask of all lines
+    all_lines = np.zeros_like(gt_mask, dtype=np.bool8)
     num_created_lines = 0
     attempts = 0
+    # Loop until the desired number of lines is created
     while num_created_lines < num_lines:
         attempts += 1
-        line = draw_line(sk_2d, gt_mask_2d, dist_to_edge=2)
+        line = draw_line(sk, gt_mask, num_lines, dist_to_edge=2)
+        # If the line is too short, skip it
         if np.sum(line) < min_line_pix:
             continue
         else:
+            # Add the line to the mask of all lines
             all_lines = np.logical_or(all_lines, line)
             num_created_lines += 1
-        if attempts > np.sum(sk_2d) * num_lines:
+        # If the number of attempts is too high, print a warning and break the loop
+        if attempts > np.sum(sk) * num_lines:
             print("Warning: Could not create all lines from the skeleton to the edge.")
             break
     return all_lines
 
-def draw_line(sk_mask_2d, gt_mask_2d, dist_to_edge=5):
+def draw_line(sk_mask, gt_mask, num_lines=5, dist_to_edge=5):
     '''
-    Take a random point on the skeleton (= True in the mask) and draw a line to the nearest edge point of the ground truth mask
+    Take a random point on the skeleton (= True in the mask) and draw a line to the nearest edge point of the ground truth mask.
     '''
     # Choose a random point from the skeleton
-    sk_coordinates = np.argwhere(sk_mask_2d)
+    sk_coordinates = np.argwhere(sk_mask)
     total_points = sk_coordinates.shape[0]
     step_size = 1 #total_points // (num_lines * 2)
     possible_points = np.arange(total_points, step=step_size)
@@ -121,9 +181,9 @@ def draw_line(sk_mask_2d, gt_mask_2d, dist_to_edge=5):
     random_point = sk_coordinates[random_point_index]
     # Erode the gt_mask, so that the line will have a distance to the edge
     if dist_to_edge:
-        eroded_gt_mask = erosion(gt_mask_2d, square(dist_to_edge*2))
+        eroded_gt_mask = erosion(gt_mask, square(dist_to_edge*2))
     else:
-        eroded_gt_mask = gt_mask_2d
+        eroded_gt_mask = gt_mask
     # Find the shortest path from the random point to the edge of the mask and return the mask of the path
     shortest_path = point_to_edge(random_point, eroded_gt_mask)
     shortest_path_mask = shortest_path == 1
